a53cf048650e1388c3bcba238d7feded6502de7e
MOHAMED Marouen <medmarouen@gmail.com>
2017-09-21 09:42:07 +0200

    Validate all test

--- src/test/java/com/medinvention/controller/HomeControllerTest.java
+++ src/test/java/com/medinvention/controller/HomeControllerTest.java
@@ -1,8 +1,10 @@
 package com.medinvention.controller;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.springframework.http.MediaType;
 
@@ -10,6 +12,7 @@ public class HomeControllerTest extends AbstractControllerTest {
 
     @Test
     public void testHome() throws Exception {
-        this.mvc.perform(get("/home").accept(MediaType.TEXT_PLAIN)).andExpect(status().isOk());
+        this.mvc.perform(get("/home").accept(MediaType.TEXT_PLAIN)).andExpect(status().isOk())
+                .andExpect(content().string(Matchers.containsString("Spring Batch")));
     }
 }
